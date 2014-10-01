bd3c8972240922cc522364e521257aa095d1f7c7
vogl <vogel91@gmx.de>
2014-10-01 22:41:42 +0200

    refactor main into another class

---
+++ src/cli/CliMain.java
@@ -0,0 +1,164 @@
+package cli;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.jfree.chart.ChartPanel;
+import org.jfree.ui.ApplicationFrame;
+
+import ratio.LRatio;
+import ratio.LongestWinSeries;
+import ratio.MovingAverage;
+import ratio.NotSoRandomWalk;
+import ratio.Rating;
+import ratio.Ratio;
+import ratio.SimpleRatio;
+import storage.DBStorage;
+import storage.Game;
+import storage.Storage;
+import storage.StorageException;
+import chart.RatioChartPanel;
+
+public class CliMain {
+	private static final String DEFAULT_MODE = "stats";
+	private static final String DEFAULT_RATIO = "lratio";
+	private static final String DEFAULT_BALANCER = "serpentine-twist";
+	private static final String DEFAULT_SERVER = "localhost/test";
+	private static final String DEFAULT_USER = "";
+	private static final String DEFAULT_PSWD = "";
+
+	public static void main(String[] args) {
+		Options options = new CliOptions();
+		try {
+			CommandLine cmd = readArgs(args, options);
+
+			if (cmd.hasOption("help")) {
+				printHelp(options);
+				System.exit(0);
+			}
+			String mode = cmd.getOptionValue("mode");
+			if (mode == null) {
+				System.out.println("Note: unkown mode. using default mode");
+				mode = DEFAULT_MODE;
+			}
+			switch (mode) {
+			case "stats":
+				statsMain(cmd);
+				break;
+			case "balance":
+				balancerMain(cmd);
+			}
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			printHelp(options);
+			System.exit(1);
+		} catch (StorageException e) {
+			System.err.println(e.getMessage());
+			System.exit(2);
+		}
+	}
+	/*================ STATS MODE ================*/
+	private static void statsMain(CommandLine cmd) throws StorageException {
+		Storage storage = getStorage(cmd);
+		Ratio ratio = getRatio(cmd);
+		List<Game> games = storage.readAllGames();
+		List<String> players = getCliPlayers(cmd);
+		if (players.size() == 0) players = storage.readAllPlayers();
+
+		printStats(players, games, ratio);
+		if (cmd.hasOption("plot"))
+			showPlot(players, games, ratio, cmd.hasOption("smooth"));
+	}
+	private static void printStats(List<String> players, List<Game> games, Ratio ratio) {
+		List<Rating> rating = ratio.get(games, players);
+		rating.sort(Collections.reverseOrder());
+		int maxLength = rating.stream().mapToInt(r -> r.player.length()).max()
+				.orElse(0);
+		rating.forEach(r -> printRating(r, maxLength));
+	}
+	private static void showPlot(List<String> players, List<Game> games, Ratio ratio, boolean smoothPlot) {
+		ChartPanel chartPanel = new RatioChartPanel(players, games, ratio,
+				smoothPlot);
+		ApplicationFrame frame = new ApplicationFrame("Ratio Development");
+		frame.setContentPane(chartPanel);
+		frame.pack();
+		frame.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) frame.dispose();
+			}
+		});
+		frame.setVisible(true);
+	}
+	private static void printRating(Rating r, int length) {
+		System.out
+				.format(Locale.ROOT, "%1$-" + (length + 1) + "s%2$.2f%n", r.player, r.rating);
+	}
+	/*================ BALANCER MODE ================*/
+	private static void balancerMain(CommandLine cmd) {
+		//Storage storage = getStorage(cmd);
+		//Ratio ratio = getRatio(cmd);
+		System.out.println("Balancer not implemented yet");
+		//Balancer balancer = getBalancer(cmd);
+		//TODO implement
+	}
+
+	/*================ HELPER ================*/
+	private static CommandLine readArgs(String[] args, Options options)
+			throws ParseException {
+		CommandLineParser parser = new GnuParser();
+		return parser.parse(options, args);
+	}
+	private static void printHelp(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("aoestats [options] [players...]", options);
+	}
+	private static Storage getStorage(CommandLine cmd) {
+		return new DBStorage(cmd.getOptionValue("server", DEFAULT_SERVER),//
+				cmd.getOptionValue("user", DEFAULT_USER),//
+				cmd.getOptionValue("paswd", DEFAULT_PSWD));
+	}
+	private static Ratio getRatio(CommandLine cmd) {
+		String ratioSystem = cmd.getOptionValue("ratio");
+		if (ratioSystem == null) {
+			System.out
+					.println("Note: unkown ratio system. using default ratio");
+			ratioSystem = DEFAULT_RATIO;
+		}
+
+		Ratio ratio = null;
+		switch (ratioSystem) {
+		case "simple":
+			ratio = new SimpleRatio();
+			break;
+		case "lratio":
+			ratio = new LRatio();
+			break;
+		case "randomwalk":
+			ratio = new NotSoRandomWalk();
+			break;
+		case "movingaverage":
+			ratio = new MovingAverage(new LRatio());
+			break;
+		case "longestwin":
+			ratio = new LongestWinSeries();
+			break;
+		default:
+			assert (false);
+		}
+		return ratio;
+	}
+	private static List<String> getCliPlayers(CommandLine cmd) {
+		return Arrays.asList(cmd.getArgs());
+	}
+}
---
+++ src/cli/CliOptions.java
@@ -0,0 +1,61 @@
+package cli;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
+@SuppressWarnings("serial")
+public class CliOptions extends Options {
+	@SuppressWarnings("static-access")
+	public CliOptions() {
+		addOption(OptionBuilder //
+				.withLongOpt("mode") //
+				.withDescription("select working mode. This is one of {stats, balance}")//
+				.hasArg() //
+				.withArgName("MODE") //
+				.withValueSeparator() //
+				.create('m'));
+
+		addOption(new Option("h", "help", false, "print this help."));
+		addOption(new Option("c", "plot", false, "plot data."));
+		addOption(new Option("n", "smooth", false, "smooth chart."));
+		addOption(new Option("d", "print", false, "print data to cmd."));
+
+		addOption(OptionBuilder //
+				.withLongOpt("ratio") //
+				.withDescription("select ratio system. This is one of {simple, lratio, randomwalk, movingaverage, longestwin}")//
+				.hasArg() //
+				.withArgName("SYSTEM") //
+				.withValueSeparator() //
+				.create('r'));
+		addOption(OptionBuilder //
+				.withLongOpt("balancer") //
+				.withDescription("select balancing system. This is one of {}")//
+				.hasArg() //
+				.withArgName("BALANCER") //
+				.withValueSeparator() //
+				.create('b'));
+
+		addOption(OptionBuilder //
+				.withLongOpt("server") //
+				.withDescription("grab games from mysql server at url") //
+				.hasArg() //
+				.withArgName("URL") //
+				.withValueSeparator() //
+				.create('s'));
+		addOption(OptionBuilder //
+				.withLongOpt("user") //
+				.withDescription("user name for mysql server") //
+				.hasArg() //
+				.withArgName("NAME") //
+				.withValueSeparator() //
+				.create("u"));
+		addOption(OptionBuilder //
+				.withLongOpt("pswd") //
+				.withDescription("password for mysql server") //
+				.hasArg() //
+				.withArgName("PASSWORD") //
+				.withValueSeparator() //
+				.create("p"));
+	}
+}
